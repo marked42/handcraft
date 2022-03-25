8008b93ba7c4f12290109b6b2dc29400a9b65aa5
penghui <penghui.zhang@foxmail.com>
2022-03-26 00:42:12 +0800

    chore: solve problem of using default import on modules with only namespace export like path/fs
    Node module like path/fs has only namespace export, so using default import on them
    requires tsconfig.json#esModuleInterop to be enabled. However, turning on this option
    inside tsconfig.json of each package instead of tsconfig.json in project root causes
    inconsistency between local eslint and vscode eslint plugin because vscode eslint plugin
    only uses config file .eslintrc in project root which references tsconfig.json in project root.
    
    So we'd better enable esModuleInterop option in tsconfig.json in project root to achieve
    consistency between local eslint and vscode eslint. So we're moving shared
    tsconfig.json/.babelrc config under project root. Furthermore, disable esModuleInterop
    is more convenient since typescript will warn us if we import incorrectly.

--- packages/eva/src/ModuleEvaluator/ModuleEvaluator.ts
+++ packages/eva/src/ModuleEvaluator/ModuleEvaluator.ts
@@ -1,5 +1,5 @@
-import path from "path";
-import fs from "fs";
+import * as path from "path";
+import * as fs from "fs";
 import { Environment } from "../Environment";
 import { CompoundExpression, ExpressionValue } from "../expression";
 import { Eva } from "../Eva";
