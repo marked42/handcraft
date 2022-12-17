c78e32ef5fe9b296bdc8bcb177f3c59e704cb2ac
zhangpenghui04 <zhangpenghui04@meituan.com>
2022-12-17 14:32:57 +0800

    refactor(mini-lisp): split arithmetic/comparison operators tests

--- packages/mini-lisp/test/interpreter.test.ts
+++ packages/mini-lisp/test/interpreter.test.ts
@@ -9,33 +9,7 @@ test("literal atom", () => {
     expect(interpret("(1 (1 2))")).toEqual([1, [1, 2]]);
 });
 
-test("built in operators", () => {
-    expect(interpret("(+ 1 2)")).toEqual(3);
-    expect(interpret("(- 1 2)")).toEqual(-1);
-    expect(interpret("(* 1 2)")).toEqual(2);
-    expect(interpret("(/ 1 2)")).toEqual(0.5);
-    expect(interpret("(= 1 2)")).toEqual(false);
-    expect(interpret("(> 1 2)")).toEqual(false);
-    expect(interpret("(>= 1 2)")).toEqual(false);
-    expect(interpret("(< 1 2)")).toEqual(true);
-    expect(interpret("(<= 1 2)")).toEqual(true);
-});
-
-test("string", () => {
-    expect(interpret('(append "hello-" "world")')).toEqual("hello-world");
-});
-
-describe("variable", () => {
-    test("throw on undefined variable", () => {
-        expect(() => interpret("a")).toThrowError();
-    });
-
-    test("read variable", () => {
-        expect(interpret("a", new Context({ a: 1 }))).toEqual(1);
-    });
-});
-
-describe("call expression", () => {
+describe("arithmetic operators", () => {
     test("+", () => {
         expect(interpret("(+ 1 2)")).toEqual(3);
     });
@@ -55,9 +29,41 @@ describe("call expression", () => {
         expect(interpret("(/ 3 0)")).toBe(Infinity);
         expect(interpret("(/ -1 0)")).toBe(-Infinity);
     });
-    // '+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv,
-    // '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq,
+});
 
+describe("comparison operators", () => {
+    test("=", () => {
+        expect(interpret("(= 1 2)")).toEqual(false);
+    });
+    test(">", () => {
+        expect(interpret("(> 1 2)")).toEqual(false);
+    });
+    test(">=", () => {
+        expect(interpret("(>= 1 2)")).toEqual(false);
+    });
+    test("<", () => {
+        expect(interpret("(< 1 2)")).toEqual(true);
+    });
+    test("<=", () => {
+        expect(interpret("(<= 1 2)")).toEqual(true);
+    });
+});
+
+test("string", () => {
+    expect(interpret('(append "hello" "world")')).toEqual("helloworld");
+});
+
+describe("variable", () => {
+    test("throw on undefined variable", () => {
+        expect(() => interpret("a")).toThrowError();
+    });
+
+    test("read variable", () => {
+        expect(interpret("a", new Context({ a: 1 }))).toEqual(1);
+    });
+});
+
+describe("call expression", () => {
     test("first", () => {
         expect(interpret("(first (1 2))")).toEqual(1);
     });
