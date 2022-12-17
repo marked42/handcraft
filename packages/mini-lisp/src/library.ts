a34c5da4390d311866e044469138ce428adac59e
zhangpenghui04 <zhangpenghui04@meituan.com>
2022-12-17 16:27:52 +0800

    feat(mini-lisp): more built-in procedures

--- packages/mini-lisp/src/library.ts
+++ packages/mini-lisp/src/library.ts
@@ -1,5 +1,7 @@
 import { ExprValue, Scope } from "../src";
 
+const PairKey = Symbol();
+
 export const StandardLibrary: Scope = {
     "+": (left: ExprValue, right: ExprValue) => {
         // TODO: duplication
@@ -9,6 +11,48 @@ export const StandardLibrary: Scope = {
         }
 
         throwInvalidOperandsError("+", ["string", "number"], [left, right]);
+    },
+    expt: (left: ExprValue, right: ExprValue) => {
+        if (typeof left === "number" && typeof right === "number") {
+            return Math.pow(left, right);
+        }
+
+        throwInvalidOperandsError("expt", ["number"], [left, right]);
+    },
+    max: (...args: ExprValue[]) => {
+        if (isNumbers(args)) {
+            return Math.max(...args);
+        }
+
+        throwInvalidOperandsError("max", ["number"], args);
+    },
+    min: (...args: ExprValue[]) => {
+        if (isNumbers(args)) {
+            return Math.min(...args);
+        }
+
+        throwInvalidOperandsError("min", ["number"], args);
+    },
+    round: (value: ExprValue) => {
+        if (typeof value === "number") {
+            return Math.round(value);
+        }
+
+        throwInvalidOperandsError("round", ["number"], [value]);
+    },
+    not: (value: ExprValue) => {
+        return !value;
+    },
+    "null?": (value: ExprValue) => {
+        return value === undefined;
+    },
+    "number?": (value: ExprValue) => {
+        return typeof value === "number";
+    },
+    print: (...args: ExprValue[]) => {
+        console.log(...args);
+        // FIXME: should return null
+        return args;
     },
     "-": (left: ExprValue, right: ExprValue) => {
         if (typeof left === "number" && typeof right === "number") {
@@ -66,6 +110,40 @@ export const StandardLibrary: Scope = {
 
         throwInvalidOperandsError(">", ["number"], [left, right]);
     },
+    abs: (left: ExprValue) => {
+        if (typeof left === "number") {
+            return Math.abs(left);
+        }
+
+        throwInvalidOperandsError(">", ["number"], [left]);
+    },
+    apply: (proc: ExprValue, ...args: ExprValue[]) => {
+        if (typeof proc !== "function") {
+            throw new Error(
+                `apply requires first parameter to be function, get ${proc.toString()}`
+            );
+        }
+        if (args.length !== 1) {
+            throw new Error(
+                `apply accepts exactly two parameters, actual ${args.toString()}`
+            );
+        }
+        if (!Array.isArray(args[0])) {
+            throw new Error(
+                `apply requires second parameter to be a list, actual ${args[0].toString()}`
+            );
+        }
+        return proc(...args[0]);
+    },
+    begin: (...args: ExprValue[]) => {
+        if (args.length === 0) {
+            throw new Error(
+                `begin accepts 1 argument at least, actual ${args.toString()}`
+            );
+        }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return args.at(-1)!;
+    },
     append: (left: ExprValue, right: ExprValue) => {
         if (typeof left === "string" && typeof right === "string") {
             return left + right;
@@ -73,14 +151,57 @@ export const StandardLibrary: Scope = {
 
         throwInvalidOperandsError("append", ["string"], [left, right]);
     },
-    first: (value: ExprValue) => {
+    car: (value: ExprValue) => {
         if (!Array.isArray(value)) {
-            throw new Error(
-                `first applied to invalid value ${value.toString()}`
-            );
+            throw new Error(`car applied to invalid value ${value.toString()}`);
         }
         return value[0];
     },
+    cdr: (value: ExprValue) => {
+        if (!Array.isArray(value)) {
+            throw new Error(`cdr applied to invalid value ${value.toString()}`);
+        }
+        return value.slice(1);
+    },
+    cons: (first: ExprValue, second: ExprValue) => {
+        const pair = [first, second];
+
+        Object.defineProperty(pair, PairKey, {
+            get value() {
+                return true;
+            },
+        });
+
+        return pair;
+    },
+    "pair?": (value: ExprValue) => {
+        return Array.isArray(value) && !!(value[PairKey as any] as boolean);
+    },
+    // TODO: not sure of exact meaning now
+    "eq?": (left: ExprValue, right: ExprValue) => {
+        return left === right;
+    },
+    "equal?": (left: ExprValue, right: ExprValue) => {
+        return left === right;
+    },
+    list: (...args: ExprValue[]) => {
+        return args;
+    },
+    "list?": (value: ExprValue) => {
+        return Array.isArray(value);
+    },
+    length: (value: ExprValue) => {
+        if (Array.isArray(value)) {
+            return value.length;
+        }
+        throw new Error(
+            `length must applies to a list or pair, received ${value.toString()}`
+        );
+    },
+    "procedure?": (value: ExprValue) => {
+        return typeof value === "function";
+    },
+    // TODO: symbol
 };
 
 function throwInvalidOperandsError(
@@ -95,4 +216,15 @@ function throwInvalidOperandsError(
             .map((e) => e.toString())
             .join(" ")})`
     );
+}
+
+function isNumbers(args: ExprValue[]): args is number[] {
+    if (
+        Array.isArray(args) &&
+        args.length > 0 &&
+        args.every((arg) => typeof arg === "number")
+    ) {
+        return true;
+    }
+    return false;
 }
--- packages/mini-lisp/test/interpreter.test.ts
+++ packages/mini-lisp/test/interpreter.test.ts
@@ -29,6 +29,11 @@ describe("arithmetic operators", () => {
         expect(interpret("(/ 3 0)")).toBe(Infinity);
         expect(interpret("(/ -1 0)")).toBe(-Infinity);
     });
+
+    test("expt", () => {
+        expect(interpret("(expt 2 3)")).toEqual(8);
+        expect(interpret("(expt 0 3)")).toEqual(0);
+    });
 });
 
 describe("comparison operators", () => {
@@ -64,8 +69,106 @@ describe("variable", () => {
 });
 
 describe("call expression", () => {
-    test("first", () => {
-        expect(interpret("(first (1 2))")).toEqual(1);
+    test("car", () => {
+        expect(interpret("(car (1 2 3))")).toEqual(1);
+        expect(() => interpret("(car )")).toThrowError();
+        expect(() => interpret("(car 1)")).toThrowError();
+    });
+
+    test("cdr", () => {
+        expect(interpret("(cdr (1 2 3))")).toEqual([2, 3]);
+        expect(() => interpret("(cdr )")).toThrowError();
+        expect(() => interpret("(cdr 1)")).toThrowError();
+    });
+
+    test("abs", () => {
+        expect(interpret("(abs 1)")).toEqual(1);
+        expect(interpret("(abs 0)")).toEqual(0);
+        expect(interpret("(abs -1)")).toEqual(1);
+    });
+
+    test("apply", () => {
+        expect(interpret("(apply + (1 2))")).toEqual(3);
+        expect(() => interpret("(apply + )")).toThrowError();
+        expect(() => interpret("(apply + (1 2) 3)")).toThrowError();
+        expect(() => interpret("(apply 1 (1 2))")).toThrowError();
+    });
+
+    test("begin", () => {
+        expect(interpret("(begin 1 2 3)")).toEqual(3);
+        expect(() => interpret("(begin)")).toThrowError();
+    });
+
+    test("length", () => {
+        expect(interpret("(length ())")).toEqual(0);
+        expect(interpret("(length (1 2 3))")).toEqual(3);
+        expect(() => interpret("(length)")).toThrowError();
+        expect(() => interpret("(length 1)")).toThrowError();
+    });
+
+    test("list", () => {
+        expect(interpret("(list 1 2 3)")).toEqual([1, 2, 3]);
+        expect(interpret("(list)")).toEqual([]);
+    });
+
+    test("list?", () => {
+        expect(interpret("(list? (list 1 2 3))")).toEqual(true);
+        expect(interpret("(list? (list))")).toEqual(true);
+        expect(interpret("(list? ())")).toEqual(true);
+        expect(interpret("(list? 1)")).toEqual(false);
+        // FIXME: treat pair as list now, needs confirm
+        expect(interpret("(list? (cons 1 2))")).toEqual(true);
+    });
+
+    test("max", () => {
+        expect(interpret("(max 1)")).toEqual(1);
+        expect(interpret("(max 1 2 3)")).toEqual(3);
+        expect(() => interpret("(max ())")).toThrowError();
+        expect(() => interpret("(max)")).toThrowError();
+    });
+
+    test("min", () => {
+        expect(interpret("(min 1)")).toEqual(1);
+        expect(interpret("(min 1 2 3)")).toEqual(1);
+        expect(() => interpret("(min ())")).toThrowError();
+        expect(() => interpret("(min)")).toThrowError();
+    });
+
+    // FIXME: need confirm
+    test("not", () => {
+        expect(interpret("(not 1)")).toEqual(false);
+    });
+
+    // FIXME: need confirm
+    test("null?", () => {
+        expect(interpret("(null? 1)")).toEqual(false);
+    });
+
+    test("number?", () => {
+        expect(interpret("(number? 1)")).toEqual(true);
+        expect(interpret("(number? ())")).toEqual(false);
+    });
+
+    test("round", () => {
+        expect(interpret("(round 1.1)")).toEqual(1);
+        expect(() => interpret("(round ())")).toThrowError();
+    });
+});
+
+describe("procedure", () => {
+    test("procedure?", () => {
+        expect(interpret("(procedure? (lambda (x) (x)))")).toEqual(true);
+        expect(interpret("(procedure? 1)")).toEqual(false);
+    });
+});
+
+describe("pair", () => {
+    test("cons", () => {
+        expect(interpret("(cons 1 2)")).toEqual([1, 2]);
+    });
+
+    test("con?", () => {
+        expect(interpret("(pair? (cons 1 2))")).toEqual(true);
     });
 });
 
