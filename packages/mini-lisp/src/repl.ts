3268ed7dfe4d8bdf4c003fdc43f76b79939fe564
zhangpenghui04 <zhangpenghui04@meituan.com>
2022-12-17 17:58:05 +0800

    feat(mini-lisp): repl & cli

---
+++ packages/mini-lisp/src/repl.ts
@@ -0,0 +1,23 @@
+import * as readline from "readline";
+import { Context } from "./context";
+import { interpret } from "./interpreter";
+import { getStandardLibrary } from "./library";
+
+import { stdin as input, stdout as output } from "process";
+
+const rl = readline.createInterface({ input, output });
+
+export function repl(prompt = "lisp > ") {
+    const rootContext = new Context(getStandardLibrary());
+    rl.setPrompt(prompt);
+    rl.prompt();
+    rl.on("line", (line: string) => {
+        try {
+            const value = interpret(line, rootContext);
+            console.log(value);
+        } catch (e) {
+            console.error((e as any)?.message ?? e);
+        }
+        rl.prompt();
+    });
+}
