a961c9ecf06e8ad218b5abcfd439f45fcd650b15
zhangpenghui04 <zhangpenghui04@meituan.com>
2022-12-21 20:56:18 +0800

    chore: remove legacy comment
    bug has been fixed

--- packages/mini-lisp/test/tokenize.test.ts
+++ packages/mini-lisp/test/tokenize.test.ts
@@ -69,24 +69,6 @@ describe("atoms", () => {
 });
 
 describe("list", () => {
-    // FIXME: debug failed
-    /**
-     * cd /Users/penghui/coding/handcraft ; /usr/bin/env 'NODE_OP
-TIONS=--require "/Applications/Visual Studio Code.app/Contents/Resources/app/extensions/ms-vs
-code.js-debug/src/bootloader.bundle.js" --inspect-publish-uid=http' 'VSCODE_INSPECTOR_OPTIONS
-={"inspectorIpc":"/var/folders/pt/_nynw_z11xx2fvyq8p8qs0fc0000gn/T/node-cdp.65570-3.sock","de
-ferredMode":false,"waitForDebugger":"","execPath":"/Users/penghui/.nvm/versions/node/v16.14.2
-/bin/node","onlyEntrypoint":false,"autoAttachMode":"always","fileCallback":"/var/folders/pt/_
-nynw_z11xx2fvyq8p8qs0fc0000gn/T/node-debug-callback-26cbd6803d3d2889"}' /Users/penghui/.nvm/v
-ersions/node/v16.14.2/bin/node ./node_modules/.bin/jest --runInBand --watchAll=false --testNa
-mePattern tokenize\ multiple\ tokens --runTestsByPath /Users/penghui/coding/handcraft/package
-s/mini-lisp/test/index.test.ts
-Debugger attached.
-Waiting for the debugger to disconnect...
-/Users/penghui/coding/handcraft/node_modules/.bin/jest:2
-basedir=$(dirname "$(echo "$0" | sed -e 's,\\,/,g')")
-          ^^^^^^^
-     */
     test("list with a symbol", () => {
         expect(tokenize("(a)")).toEqual([
             { type: TokenType.Punctuator, source: "(" },
