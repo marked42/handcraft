9910a6458b8655c5096a6c2da5b1eb1e5da09f17
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-03-07 14:04:55 +0800

    feat: call by reference

---
+++ EOPL/call-by-reference.rkt
@@ -0,0 +1,325 @@
+#lang eopl
+
+(require rackunit)
+(require "ch2.rkt")
+(require "store.rkt")
+
+(define-datatype program program?
+  (a-program (exp1 expression?))
+  )
+
+(define-datatype expression expression?
+  (const-exp
+   (num number?))
+  (diff-exp
+   (exp1 expression?)
+   (exp2 expression?)
+   )
+  (zero?-exp
+   (exp1 expression?)
+   )
+  (if-exp
+   (exp1 expression?)
+   (exp2 expression?)
+   (exp3 expression?)
+   )
+
+  (var-exp
+   (var identifier?))
+  (assign-exp
+   (var identifier?)
+   (exp1 expression?)
+   )
+
+  (let-exp
+   (var identifier?)
+   (exp1 expression?)
+   (body expression?)
+   )
+
+  (begin-exp
+    (exp1 expression?)
+    (exps (list-of expression?))
+    )
+
+  (proc-exp
+   (var identifier?)
+   (body expression?)
+   )
+  (call-exp
+   (rator expression?)
+   (rand expression?)
+   )
+  )
+
+(define identifier? symbol?)
+
+(define-datatype expval expval?
+  (num-val (num number?))
+  (bool-val (bool boolean?))
+  (proc-val (proc proc?))
+  (ref-val (ref reference?))
+  )
+
+(define (expval->num val)
+  (cases expval val
+    (num-val (num) num)
+    (else (report-expval-extractor-error 'num val))
+    )
+  )
+
+(define (expval->proc val)
+  (cases expval val
+    (proc-val (proc) proc)
+    (else (report-expval-extractor-error 'proc val))
+    )
+  )
+
+(define (expval->bool val)
+  (cases expval val
+    (bool-val (bool) bool)
+    (else (report-expval-extractor-error 'bool val))
+    )
+  )
+
+(define (expval->ref val)
+  (cases expval val
+    (ref-val (ref) ref)
+    (else (report-expval-extractor-error 'ref val))
+    )
+  )
+
+(define empty-env '())
+(define (init-env)
+  (extend-env
+   'i (num-val 1)
+   (extend-env 'v (num-val 5)
+               (extend-env 'x (num-val 10)
+                           empty-env)
+               )
+   )
+  )
+
+(define (report-expval-extractor-error type val)
+  (eopl:error 'expval-extractors "Looking for a ~s, found ~s" type val)
+  )
+
+(define (run string)
+  (value-of-program (scan&parse string))
+  )
+
+(define (value-of-program prog)
+  (initialize-store!)
+  (cases program prog
+    (a-program (exp1)
+               (value-of exp1 (init-env))
+               )
+    )
+  )
+
+(define (value-of exp env)
+  (cases expression exp
+    (const-exp (num) (num-val num))
+    ; 所有变量都是ref
+    (var-exp (var) (deref (apply-env env var)))
+    (assign-exp (var exp1)
+                (begin
+                  (setref!
+                   (apply-env env var)
+                   (value-of exp1 env)
+                   )
+                  ; return arbitrary val
+                  (num-val 27)
+                  )
+                )
+    (diff-exp (exp1 exp2)
+              (let ((val1 (value-of exp1 env))
+                    (val2 (value-of exp2 env)))
+                (let ((num1 (expval->num val1))
+                      (num2 (expval->num val2))
+                      )
+                  (num-val (- num1 num2))
+                  )
+                )
+              )
+    (zero?-exp (exp1)
+               (let ((val1 (value-of exp1 env)))
+                 (let ((num1 (expval->num val1)))
+                   (if (zero? num1)
+                       (bool-val #t)
+                       (bool-val #f)
+                       )
+                   )
+                 )
+               )
+    (if-exp (exp1 exp2 exp3)
+            (let ((val1 (value-of exp1 env)))
+              (if (expval->bool val1)
+                  (value-of exp2 env)
+                  (value-of exp3 env)
+                  )
+              )
+            )
+
+    (let-exp (var exp1 body)
+             (let ((val (value-of exp1 env)))
+               (value-of body
+                         (extend-env var (newref val) env)
+                         ))
+             )
+
+    (begin-exp (exp1 exps)
+               (letrec
+                   ((value-of-begins
+                     (lambda (e1 es)
+                       (let ((v1 (value-of e1 env)))
+                         (if (null? es)
+                             v1
+                             (value-of-begins (car es) (cdr es)))))))
+                 (value-of-begins exp1 exps)))
+
+    (proc-exp (var body)
+              (proc-val (procedure var body env))
+              )
+    (call-exp (rator rand)
+              (let ((proc (expval->proc (value-of rator env)))
+                    (arg (value-of-operand rand env))
+                    )
+                (apply-procedure proc arg)
+                )
+              )
+    )
+  )
+
+; represent procedure as custom data structure
+(define-datatype proc proc?
+  (procedure
+   (var identifier?)
+   (body expression?)
+   (saved-env environment?)
+   )
+  )
+
+(define (apply-procedure proc1 val)
+  (cases proc proc1
+    (procedure (var body saved-env)
+               (value-of body (extend-env var val saved-env))
+               )
+    )
+  )
+
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program (expression) a-program)
+
+    (expression (number) const-exp)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
+
+    (expression
+     ("zero?" "(" expression ")")
+     zero?-exp)
+
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
+
+    (expression (identifier) var-exp)
+
+    (expression
+     ("let" identifier "=" expression "in" expression)
+     let-exp)
+
+    (expression
+     ("begin" expression (arbno ";" expression) "end")
+     begin-exp)
+
+    (expression
+     ("proc" "(" identifier ")" expression)
+     proc-exp
+     )
+
+    (expression
+     ("(" expression expression ")")
+     call-exp
+     )
+
+    (expression
+     ("set" identifier "=" expression)
+     assign-exp
+     )
+    ))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+
+(define equal-answer?
+  (lambda (ans correct-ans msg)
+    (check-equal? ans (sloppy->expval correct-ans) msg)))
+
+(define sloppy->expval
+  (lambda (sloppy-val)
+    (cond
+      ((number? sloppy-val) (num-val sloppy-val))
+      ((boolean? sloppy-val) (bool-val sloppy-val))
+      (else
+       (eopl:error 'sloppy->expval
+                   "Can't convert sloppy value to expval: ~s"
+                   sloppy-val)))))
+
+; call-by-reference only in apply-procedure
+; let-exp and assign-exp make a new variable
+; 1. var-ref deref to get actual value
+; 1. let-exp variable with initialization value
+; 1. assign-exp update ref value with assigned value
+; 1. apply-procedure pass ref directly when operand is var-exp, new ref otherwise.
+(define (value-of-operand exp env)
+  (cases expression exp
+    (var-exp (var) (apply-env env var))
+    (else (newref (value-of exp env)))
+    )
+  )
+
+(equal-answer? (run "
+let x = 1
+  in let y = x
+   in begin
+     set y = 3;
+     y
+   end
+") 3 "update y")
+
+(equal-answer? (run "
+let x = 1
+  in let y = x
+   in begin
+     set y = 3;
+     x
+   end
+") 1 "x and y are two standalone variable, changing y doesn't affect x")
+
+(equal-answer? (run "
+let swap = proc (x) proc (y)
+  let temp = x
+  in begin
+    set x = y;
+    set y = temp
+  end
+ in let a = 33
+  in let b = 44
+    in begin
+      ((swap a) b);
+      -(a, b)
+    end
+") 11 "call by reference, swap two variables")
